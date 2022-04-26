081974280e795c28391d20118e83e799575f2e90
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-04-26 02:53:35 +0200

    Added Tiger Compiler project page and a few other features

---
+++ pages/projects/tiger.js
@@ -0,0 +1,46 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, Meta } from '../../components/project'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+import { ProjectImage } from '../../components/project'
+
+const Project = () => {
+    return (
+        <Layout title="Tiger Compiler">
+            <Container>
+                <Title>
+                    Tiger Compiler {' '}<Badge>2022</Badge>
+                </Title>
+                <P>
+                    Frontend of a compiler for the toy language Tiger, as described in the book &apos;Modern Compiler Implementation&apos;
+                    by Andrew Appel. It was written in C++ within a group of 4 students.
+                </P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Source code</Meta>
+                        <Link href='https://gitfront.io/r/ShinysArc/8TSZtYkpsPHy/TC/' target="_blank">
+                            Link<ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Tags</Meta>
+                        <span>
+                            <Link href='https://en.wikipedia.org/wiki/C%2B%2B' target="_blank">
+                                C++
+                            </Link>, {' '}
+                            <Link href='https://en.wikipedia.org/wiki/GNU_Autotools' target="_blank">
+                                Autotools
+                            </Link>, {' '}
+                            <Link href='https://www.lrde.epita.fr/~tiger/tiger.html' target="_blank">
+                                Tiger
+                            </Link>
+                        </span>
+                    </ListItem>
+                </List>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Project
