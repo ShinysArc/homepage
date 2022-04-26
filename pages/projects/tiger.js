372335b3c914a4432987b864d8abccda4dd882b6
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-04-26 02:55:11 +0200

    Fixed unused variable

--- pages/projects/tiger.js
+++ pages/projects/tiger.js
@@ -3,7 +3,6 @@ import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, Meta } from '../../components/project'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
-import { ProjectImage } from '../../components/project'
 
 const Project = () => {
     return (
