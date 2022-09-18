13434d20ed0fdff084a560c5c6738bdace490f49
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-09-18 19:22:08 +0200

    Added meta description.

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,6 +8,7 @@ const Main = ({ children, router }) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+	    	<meta name="description" content="Stephane Gelibert's personal website."/>
                 <link rel="shortcut icon" href="/favicon.png" />
                 <title>Stephane Gelibert</title>
             </Head>
