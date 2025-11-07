22c2072e1adb4a6fac3331bfc6437a201f66e7b3
Stéphane Gelibert <contact@stephanegelibert.com>
2025-11-07 23:34:41 +0100

    gngng

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const { i18n } = require('./next-i18next.config.js')
 
 module.exports = {
     output: 'standalone',
-    swcMinify: true,
     experimental: {
         outputStandalone: true
     },
